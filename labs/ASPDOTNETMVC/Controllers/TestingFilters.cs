c945b07424bbdbfa86ab4a6070be2232e1bef6f6
Maiwand Hussain <mhussain2@spartaglobal.com>
2019-03-21 16:49:06 +0000

    many projects and changes

---
+++ labs/ASPDOTNETMVC/Controllers/TestingFilters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASPDOTNETMVC.Controllers
+{
+    public class TestingFilters : ActionFilterAttribute
+    {
+        
+        private void LoggingMethod(RouteData rd)
+        {
+            var controller = rd.Values["controller"];
+            var action = rd.Values["action"];
+            string message = string.Format("Controller: {0}: Action: {1}", controller, action);
+            Debug.Write(message);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            LoggingMethod(filterContext.RouteData);
+        }
+    }
+}
